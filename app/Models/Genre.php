cf99b5fd6f6d6d32b576e643d92271528021d872
Camille Marcq <camille.marcq2@gmail.com>
2021-05-25 14:13:22 +0200

    14h13_cam

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Photo extends Model
+class Genre extends Model
 {
     use HasFactory;
-    protected $table = "photos";
+    protected $table = "genres";
 
     protected $fillable = ["nom"];
 }
--- database/migrations/2021_05_25_120943_create_genres_table.php
+++ database/migrations/2021_05_25_120943_create_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContinentsTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateContinentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('continents', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->id();
             $table->string("nom", 255);
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateContinentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('continents');
+        Schema::dropIfExists('genres');
     }
 }
