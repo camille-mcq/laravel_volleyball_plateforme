ab727aa7927e16d0eecedf1ef074866e7a0894a5
Laurie <laurieiula2016@gmail.com>
2021-05-25 16:02:05 +0200

    structure page home

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,29 @@
+<ul class="nav flex-column h-100">
+    <li class="nav-item">
+      <a class="nav-link active home" aria-current="page" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Equies Complètes</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Equipes incomplètes</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Joueurs sans équipe</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Joueurs avec équipe</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Equipes Europe</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Equipes hors Europe</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Joueuse(femmes) avec équipe</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Joueur(hommes) avec équipe</a>
+    </li>
+</ul>
